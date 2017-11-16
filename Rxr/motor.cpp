28bb114cfd979290028b72c15122f960499e37f1
LoneWolf367 <lonewolf367@gmail.com>
2017-11-16 15:30:51 -0800

    Revert "v2 release - added drv8880 support"
    This reverts commit 8927204d7c96a636ae24d76e469b7797b17ec1f7.

--- Rxr/motor.cpp
+++ Rxr/motor.cpp
@@ -1,39 +1,27 @@
 #include "motor.h"
 #include "macros.h"
 
-#define NOP __asm__ __volatile__ ("nop\n\t")
-
 void motor_pulse() {
-  STEP_PIN(SET); // easydriver
-  STEP_PIN(CLR);
-  
-  STEP2_PIN(SET); // drv8880
-  NOP;NOP; // this seems to smooth out the steps
-  STEP2_PIN(CLR);
+    STEP_PIN(SET);
+    STEP_PIN(CLR);
 }
 
 void motor_set_dir_forward() {
-  DIR_PIN(SET); // easydriver
-  DIR2_PIN(SET); // drv8880
+    DIR_PIN(SET);
 }
 
 void motor_set_dir_backward() {
-  DIR_PIN(CLR); // easydriver
-  DIR2_PIN(CLR); // drv8880
+    DIR_PIN(CLR);
 }
 
 void motor_sleep() {
-  SLEEP_PIN(CLR); // easydriver
-  SLEEP2_PIN(CLR); // drv8880
+    SLEEP_PIN(CLR);
 }
 
 void motor_wake() {
-  SLEEP_PIN(SET); // easydriver
-  SLEEP2_PIN(SET); // drv8880
+    SLEEP_PIN(SET);
 }
 
-
-// easydriver microsteps
 void motor_set_steps(int steps) {
     switch (steps) {
         case FULL_STEPS: {
