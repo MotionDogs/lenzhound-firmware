5ade42cb01b10857a8920daf56acfaab095e1c65
LoneWolf367 <daniel@motiondogs.com>
2017-11-16 15:56:50 -0800

    v2 Proper Version Bump
    Properly versioned.
    Binary included.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ UNAME_STR=`uname`
 if [ "$UNAME_STR" == 'Darwin' ]; then
 	curl https://downloads.arduino.cc/arduino-1.8.1-macosx.zip > ~/.lenzhound/arduino.zip
 	unzip ~/.lenzhound/arduino.zip -d ~/.lenzhound
-elif [ "$UNAME_STR" == 'Linux' ];
+elif [ "$UNAME_STR" == 'Linux' ]; then
 	curl https://downloads.arduino.cc/arduino-1.8.1-linux64.tar.xz > ~/.lenzhound/arduino.tar.xz
 	tar -xf ~/.lenzhound/arduino.tar.xz ~/.lenzhound/arduino
 else
